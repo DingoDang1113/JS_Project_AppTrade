fef66724b2b79afbbf51c6454130ae8380332ba1
DingoDang1113 <Shalpeng109@gmail.com>
2023-09-28 14:49:13 -0700

    update

--- src/scripts/charts.js
+++ src/scripts/charts.js
@@ -1,26 +1,4 @@
 
-export function buildChart(labels, gainLosses) {
-    const ctx = document.getElementById('myChart').getContext('2d');
-  
-    const myChart = new Chart(ctx, {
-      type: 'bar', 
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Gain/Loss in USD',
-          data: gainLosses,
-          // ... other chart properties
-        }]
-      },
-      options: {
-        // ... chart options
-      },
-    });
-  
-    return myChart;
-  }
-  
-
   
   export function buildPieChart(labels, percentageContributions, colors) {
     const ctx = document.getElementById('pieChart').getContext('2d');
